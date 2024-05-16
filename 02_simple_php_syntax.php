47ef25309255775ac4aebb65ab2e1b5c6f820774
Aerrol-Kyle <166883139+Aerrol-Kyle@users.noreply.github.com>
2024-05-16 21:29:36 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php
@@ -0,0 +1,37 @@
+<?php
+
+
+$name = 'Aerrol Kyle Santos';
+$age = 20;
+$address = 'Purok 2, Brgy Manacnac, Palayan City, Nueva Ecija';
+// $hobbies = 'Drawing, Playing Guitar, Basketball, Napping, Exploring';
+// $pet_peeve = 'Fake People';
+
+
+echo $name . "</br>";
+echo $age . "</br>";
+echo $address . "</br>";
+echo $hobbies . "</br>";
+echo $pet_peeve . "</br>";
+
+
+define("Name", "Aerrol Kyle Santos");
+define("Age", 20);
+define("Address", "Purok 2, Brgy Manacnac, Palayan City, Nueva Ecija");
+define("Hobbies", "Drawing, Playing Guitar, Basketball, Napping, Exploring");
+define("Pet_Peeve", "Fake People");
+
+
+print Name . "</br>";
+print Age . "</br>";
+print Address . "</br>";
+print Hobbies . "</br>";
+print Pet_Peeve . "</br>";
+
+
+var_dump(Name, Age, Address, Hobbies, Pet_Peeve);
+
+
+/*
+Because people who are fake are worse. Imagine, y'all having fun and stuff and when you turn your back they have a lot of bad things to say about you.
+*/
